43f7e78c971ae8e047dd5a55dd745eb0b82a8886
zoeobertt <zoeobertt@gmail.com>
2025-03-08 20:41:28 -0300

    Guardando cambios antes del rebase

---
+++ Modulo2/Modulo1/ejercicio3.js
@@ -0,0 +1,13 @@
+//Ejercicio 3: Suma de dos números ingresados por el usuario
+// Enunciado: Este ejercicio te ayudará a practicar la entrada de datos y el uso de operadores aritméticos. Escribe un programa que:
+// 1. Pida al usuario que ingrese dos números.
+// 2. Sume ambos números.
+// 3. Muestre el resultado de la suma.
+// Requisitos:
+// • Utiliza prompt() para recibir los números.
+// • Utiliza parseFloat() para convertir las entradas a números.
+// • Almacena los resultados en variables adecuadas y muestra el resultado
+
+const prompt = require('prompt-sync')();
+
+let numero = parseFloat(prompt("Ingresa un numero:")); // Pida al usuario que ingrese un número.
